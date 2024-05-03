ca6aeaaeb6f20dbed1baaf2c7c8e0ecd7cbd6848
Elias <101352052+eliascending@users.noreply.github.com>
2024-05-03 11:40:42 +0200

    page home ok

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,22 @@
+<script>
+    import { onMount } from 'svelte';
+    let pokemons = [];
+
+    onMount(async () => {
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon');
+        const data = await response.json();
+        pokemons = data.results.map(pokemon => ({
+        ...pokemon,
+        imageUrl: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${getIdFromUrl(pokemon.url)}.png`
+        }));
+    });
+
+    function getIdFromUrl(url) {
+        const parts = url.split('/');
+        return parts[parts.length - 2];
+    }
+</script>
+
 <section>
     <div>
         <form>
@@ -6,5 +25,15 @@
                 Rechercher
             </button>
         </form>
+    </div>
+    <div class="pokemons">
+        <ul>
+            {#each pokemons as pokemon}
+                <li>
+                    <img src={pokemon.imageUrl} alt={pokemon.name} />
+                    <span>{pokemon.name}</span>
+                </li>
+            {/each}
+        </ul>
     </div>
 </section>
