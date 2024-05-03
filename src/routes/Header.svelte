34d7c5846f70039b3659d0d88eee1e330f090b5a
Elias <101352052+eliascending@users.noreply.github.com>
2024-05-03 10:20:11 +0200

    premier

---
+++ src/routes/Header.svelte
@@ -0,0 +1,37 @@
+<script>
+
+</script>
+<header>
+    <nav>
+        <ul>
+            <li>
+                Pokepupuce
+            </li>
+            <li>
+                Trouve ta pupuce.net
+            </li>
+        </ul>
+    </nav>
+</header>
+<style>
+header {
+    width: 100vw;
+    height: 5vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+nav {
+    width: 80%;
+    height: 100%;
+}
+ul {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    list-style: none;
+}
+ul li {
+    list-style: none;
+}
+</style>
